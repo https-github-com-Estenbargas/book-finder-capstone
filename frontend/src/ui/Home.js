c07f933c4ba2dbe746a7601509246e48b3aeb0b1
Esten Bargas <ebargas1@cnm.edu>
2021-11-15 10:51:38 -0600

    quick fixes

--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -4,7 +4,7 @@ import {Link} from "react-router-dom";
 export const Home = () => {
     return (
         <>
-            <Link exact to={"/login"}>
+            <Link to={"/login"}>
                 <h1>Home</h1>
             </Link>
 
--- frontend/src/ui/Login.js
+++ frontend/src/ui/Login.js
@@ -1,5 +1,6 @@
 import React from "react"
 import {LoginForm} from "./shared/components/LoginForm";
+
 export const LoginPage = () => {
     return (
         <>
