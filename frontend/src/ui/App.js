23bca3d07b459ff903583e42383144780f7d7232
Joshua Garvin <jgarvin6@cnm.edu>
2021-11-15 14:21:53 -0600

    added library directory and Library.js folder

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router'
 import { Home } from './Home'
 import { FourOhFour } from './FourOhFour'
 import React from 'react'
+import {Library} from "./library-page/Library";
 
 export const App = () => (
     <>
         <BrowserRouter>
             <Switch>
                 <Route exact path='/' component={Home} />
+                <Route exact path='/library' component={Library}/>
                 <Route component={FourOhFour} />
             </Switch>
         </BrowserRouter>
---
+++ frontend/src/ui/library-page/Library.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export const Library = () => {
+    return (
+        <>
+            <h1>Library Page</h1>
+        </>
+    )
+}
