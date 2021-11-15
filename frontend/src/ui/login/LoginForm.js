08b57bfc8adca242082c2b3fe8abd8c115a20db5
Esten Bargas <ebargas1@cnm.edu>
2021-11-15 17:10:51 -0600

    changed colors for background and componets, Fixed sizing issues with form

--- frontend/src/ui/login/LoginForm.js
+++ frontend/src/ui/login/LoginForm.js
@@ -1,13 +1,14 @@
 import React from "react"
 import {Button, Container} from "react-bootstrap";
 import "./Login.css"
+import {Link} from "react-router-dom";
 
 export const LoginForm = () => {
     return (
         <>
 
                 <Container fluid className={"min-vh-100 min-vw-100 d-flex align-items-center justify-content-center"} >
-                    <form className={"d-flex flex-column p-5"} id={"form-wrapper"}>
+                    <form className={"d-flex flex-column p-5 text-white bg-dark"} id={"form-wrapper"}>
                         <h1 className={"text-center"}>Sign In</h1>
 
                         <label className={"input-labels my-1"}>
@@ -18,7 +19,11 @@ export const LoginForm = () => {
                             <h2>Password</h2>
                             <input type="text" className={"p-1 my-1"}/>
                         </label>
-                        <Button className={"w-25 my-1"}>a</Button>
+                        <div className={"d-flex align-items-center"}>
+                            <Button id={"form-button"} className={"my-1"}>Submit</Button>
+                            <p className={"text-center ms-5 my-0"}> Don't have an account? <Link to="/sign-up">sign up</Link></p>
+                        </div>
+
                     </form>
                 </Container>
 
