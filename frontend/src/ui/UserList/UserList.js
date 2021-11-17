16a27137e6a0c4d9ece7262e523580713a9f13a2
Esten Bargas <ebargas1@cnm.edu>
2021-11-17 14:57:55 -0600

    added placeholders to columns

--- frontend/src/ui/UserList/UserList.js
+++ frontend/src/ui/UserList/UserList.js
@@ -1,9 +1,10 @@
-import {Col, Container, Row} from "react-bootstrap";
+import {Col, Container, Image, Row} from "react-bootstrap";
 import {BackToHomeSideBar, HomeSideBar} from "../shared/components/SideBars";
 import {BackToHomeOffCanvasSideBar, HomeOffCanvasSideBar} from "../shared/components/OffCanvasSideBar";
 import React from "react";
 import {MainNav} from "../shared/components/NavBar";
-
+import Placeholder from "../shared/imgs/placeholder-profileimg.png";
+import "./UserList.css"
 export const UserList = () => {
     return (
         <>
@@ -13,9 +14,10 @@ export const UserList = () => {
                     <BackToHomeSideBar/>
                     <BackToHomeOffCanvasSideBar/>
                     <Col xs={'10'} className={'bg-secondary'} id={'content-wrapper'}>
-                        {Array.from({ length: 8}).map((_, idx) => (
-                        <Row>
-                            1
+                        {Array.from({ length: 9}).map((_, idx) => (
+                        <Row className={"border-top border-dark"}>
+                            <Image className={"user-images rounded-circle p-3"} src={Placeholder} alt={"placeholder"} width={"30"} height={"30"} />
+                            <p>PlaceHolder Text</p>
                         </Row>
                         ))}
                     </Col>
