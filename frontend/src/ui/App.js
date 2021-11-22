0284146a752d62c942746cdf08e4145794db536c
Emily Williams <ewilliams79@cnm.edu>
2021-11-22 08:15:45 -0600

    Created Signup.js and path on App.js.

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Route, Switch } from 'react-router'
 import { Home } from './Home/Home'
+import { Signup } from './Signup/Signup'
 import { FourOhFour } from './FourOhFour'
 import React from 'react'
 import "./App.css"
@@ -12,6 +13,7 @@ export const App = () => (
         <BrowserRouter>
             <Switch>
                 <Route exact path='/' component={Home} />
+                <Route exact path='/sign-up' component={Signup} />
                 <Route component={FourOhFour} />
             </Switch>
         </BrowserRouter>
