bb4625da84764136c437ed7d5c3cb8157f3fe5e0
Emily Williams <ewilliams79@cnm.edu>
2021-11-30 11:01:20 -0600

    Initial sign in controller.

--- backend/src/apis/sign-in/sign-in.controller.ts
+++ backend/src/apis/sign-in/sign-in.controller.ts
@@ -7,3 +7,56 @@ import {generateJwt, validatePassword} from "../../utils/auth.utils";
 import {User} from "../../utils/interfaces/User";
 import {selectUserByUserEmail} from "../../utils/user/selectUserByUserEmail";
 
+export async function signInController(request: Request, response: Response, nextFunction: NextFunction) : Promise<Response| undefined> {
+    try {
+        const {userPassword} = request.body
+
+        passport.authenticate(
+            "local",
+            {session: false},
+            async (err: any, passportUser: User) => {
+                console.log(passportUser)
+                const {userId, userEmail, userName} = passportUser;
+                const signature: string = uuid();
+                const authorization: string = generateJwt ({userId, userEmail, userName}, signature);
+
+                const signInFailed = (message: string) => response.json({
+                    status: 400,
+                    data: null,
+                    message
+                });
+                const signInSuccessful = () => {
+                    if(passportUser.userActivationToken !== null) {
+                        signInFailed("Please activate your account.")
+                    }
+                    if (request.session) {
+                        request.session.user = passportUser;
+                        request.session.jwt = authorization;
+                        request.session.signature = signature;
+                    }
+                    response.header({
+                        authorization
+                    });
+
+                    return response.json({status: 200, data: null, message: "sign in successful"})
+                }
+            }
+        )
+    }
+    const LocalStrategy = passportLocal.Strategy;
+
+    export const passportStrategy: Strategy = new LocalStrategy(
+        {
+            usernameField: "userEmail",
+            passwordField: "userPassword"
+        },
+        async (email, password, done) => {
+            try {
+                const user: User| null = await selectUserByUserEmail(email);
+                return user ? done(null, user) : done(undefined, undefined, {message: "Incorrect username or password."});
+            } catch (error) {
+                return done(error);
+            }
+        }
+    );
+}
