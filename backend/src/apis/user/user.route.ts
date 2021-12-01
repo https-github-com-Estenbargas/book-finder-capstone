5bacb3d83018af0ada8188a9cb79d3aa31729418
Esten Bargas <ebargas1@cnm.edu>
2021-12-01 14:13:21 -0600

    pulled development branch into create-user-restapi

--- backend/src/apis/user/user.route.ts
+++ backend/src/apis/user/user.route.ts
@@ -3,7 +3,15 @@ import {check, checkSchema} from "express-validator";
 import {userVaildator} from "./user.vaildator";
 import {asyncValidatorController} from "../../utils/controllers/asyncVaildator.controller"
 import {getUserByUserId, getPartialUserByUserId, putUserController} from "./user.controller";
+import {isLoggedIn} from "../../utils/controllers/isLoggedIn.controller"
 
 export const UserRoute: Router = Router()
 UserRoute.route("/")
-    .post
+    .post(putUserController);
+
+UserRoute.route("/:userId")
+    .get(
+        asyncValidatorController([check("userId", "Please Provide A Valid UserId").isUUID()])
+        , getUserByUserId
+    )
+    .put(isLoggedIn, asyncValidatorController(checkSchema(userVaildator)), putUserController)
