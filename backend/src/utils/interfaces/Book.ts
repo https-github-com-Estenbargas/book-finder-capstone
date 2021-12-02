a0bcbc74d1ebe1a0b80075847a5ac8942ba0f26f
Emily Williams <ewilliams79@cnm.edu>
2021-12-02 13:21:29 -0600

    Added Visited and Book interfaces.

---
+++ backend/src/utils/interfaces/Book.ts
@@ -0,0 +1,10 @@
+export interface Book {
+    bookId : string|null,
+    bookAuthor : string,
+    bookDescription : string,
+    bookGenre : string,
+    bookImage : string,
+    bookIsbn : string,
+    bookPublisher : string,
+    bookTitle : string
+}
