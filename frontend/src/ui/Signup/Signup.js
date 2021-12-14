9f989946516b06675dbddcbbff4b93354d574d68
Esten Bargas <ebargas1@cnm.edu>
2021-12-13 21:59:38 -0600

    styled signup and signin

--- frontend/src/ui/Signup/Signup.js
+++ frontend/src/ui/Signup/Signup.js
@@ -1,12 +1,21 @@
 import React from "react"
 import {MainNav} from "../shared/components/NavBar.js"
 import {PostForm} from "./SignupForm"
+import {Container} from "react-bootstrap";
 
 export const Signup = () => {
     return (
         <>
             <MainNav />
-            <PostForm />
+            <Container fluid className={"d-flex justify-content-center align-items-center min-vh-100 w-auto"}>
+            <Container className={"p-5 bg-dark"}>
+                        <h1 className={"text-light"}>Sign Up</h1>
+                <PostForm />
+
+            </Container>
+            ></Container>
+
+
         </>
     )
 }
